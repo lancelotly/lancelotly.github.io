1ed842d1a1282a0f452ee8f700ad11626b9cae90
lancelotly <nmqsly-a@126.com>
2015-04-19 22:54:55 -0600

    Initial commit

---
+++ final.py
@@ -0,0 +1,14 @@
+AUTHOR = 'lancelotly'
+SITENAME = u'Lancelot\xb7Sirius\xb7ly'
+SITEURL = 'http://lancelotly.ml'
+
+GOOGLE_ANALYTICS='UA-57996371-5'
+TIMEZONE = 'Asia/Shanghai'
+SOCIAL = (('twitter', 'http://twitter.com/lancelotly'),
+          #('github', ''),
+          #('weibo', 'http://weibo.com/lancelotly'),
+         )
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+
+#THEME=''

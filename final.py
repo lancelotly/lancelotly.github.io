4b6ba7821f6c894552b4b4354d36e28e64eb36e0
lancelotly <nmqsly-a@126.com>
2015-04-20 11:30:59 -0600

    Initial commit

--- final.py
+++ final.py
@@ -1,14 +1,34 @@
 AUTHOR = 'lancelotly'
 SITENAME = u'Lancelot\xb7Sirius\xb7ly'
-SITEURL = 'http://lancelotly.github.io'
+SITEURL = 'http://lancelotly.me.ly'
+
+#TEMPLATE_PAGES = {'blog.html': 'blog.html'}
+#STATIC_PATHS = ['images']
 
 GOOGLE_ANALYTICS='UA-57996371-5'
 TIMEZONE = 'Asia/Shanghai'
+DEFAULT_LANG = u'zh'
+
 SOCIAL = (('twitter', 'http://twitter.com/lancelotly'),
           #('github', ''),
           #('weibo', 'http://weibo.com/lancelotly'),
+          ('envelope', 'mailto:nmqsly_b@126.com'),
          )
-FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
+FEED_RSS = 'feed/index.html'
+FEED_ATOM = 'feed/atom/index.html'
 
-#THEME=''
+#PAGINATED_DIRECT_TEMPLATES = ('blog-index',)
+#DIRECT_TEMPLATES = ('categories', 'blog-index', 'blog')
+
+#DEFAULT_DATE_FORMAT = ('%d/%b/%Y %a')
+
+#ARTICLE_URL = "{date:%Y}/{date:%m}/{slug}/"
+#ARTICLE_SAVE_AS = "{date:%Y}/{date:%m}/{slug}/index.html"
+
+#PLUGIN_PATH = 'plugins'
+#PLUGINS = ['sitemap', 'neighbors', 'related_posts']
+#RELATED_POSTS_MAX = 3
+
+THEME = 'simple'
+
+DESCRIPTION = """Here I'm searching the fantastic nature of our world."""
